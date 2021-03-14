238b12ac86bff8d5108de98bd6f30f72cc453ce2
Margarita Popova <margarita.kistanova@gmail.com>
2021-03-14 16:36:03 +0600

    return after alert

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function compute()
     var principal = document.getElementById("principal").value;
     if (principal == 0 || principal < 0) {
         alert("Please enter a number that is greater than 0")
+        return;
     }
     var rate = parseFloat(document.getElementById("range").value);
     var year = parseInt(document.getElementById("years").value);
